e995790d139917f688246b1409076f2d2f198099
MatthewMawby <themesta@gmail.com>
2016-11-21 18:44:55 -0500

    checks messages before operating on them & prevents players from playing two tictactoe moves in a row

--- botler.js
+++ botler.js
@@ -1,6 +1,7 @@
 var login = require("facebook-chat-api")
 var board = ['- - -','- - -','- - -']
 var moveCount = 0;
+var prev_player = null;
 
 //Logs in and receives commands
 function main(){
@@ -9,62 +10,64 @@ function main(){
 
         //begin listening for messages
         api.listen(function callback(err, message){
-            var args = message.body.split(' ');
-            switch (args[0].toLowerCase()){
-                //lists all botler commands & usage
-                case ".help":
-                    var send = "```\n'.help' : prints this menu\n\n";
-                    send+="-----------------------\n";
-                    send+= "'.about' : An introduction to the bot\n\n";
-                    send+="-----------------------\n";
-                    send+="'.google' : Googles whatever you type following the command\n\n";
-                    send+="-----------------------\n";
-                    send+="'.tictac' : Allows you to play tictactoe\n\n";
-                    send+="  '.tictac -print'\n displays the current state of the board\n\n";
-                    send+="  '.tictac -play X/O Xpos Ypos'\n plays the chosen letter at the given coordinate\n\n";
-                    send+="  ',tictac -clear'\n resets the board\n\n";
-                    send+="-----------------------\n";
-                    send+="'.random min max' : Chooses a random number in the given range\n\n";
-                    send+="-----------------------\n";
-                    send+="'.choose opt1 opt2 ... optn' : randomly chooses an option from the provided arguments";
-                    api.sendMessage(send, message.threadID);
-                    break;
+            if (typeof message != undefined && typeof message.body != undefined){
+                var args = message.body.split(' ');
+                switch (args[0].toLowerCase()){
+                    //lists all botler commands & usage
+                    case ".help":
+                        var send = "```\n'.help' : prints this menu\n\n";
+                        send+="-----------------------\n";
+                        send+= "'.about' : An introduction to the bot\n\n";
+                        send+="-----------------------\n";
+                        send+="'.google' : Googles whatever you type following the command\n\n";
+                        send+="-----------------------\n";
+                        send+="'.tictac' : Allows you to play tictactoe\n\n";
+                        send+="  '.tictac -print'\n displays the current state of the board\n\n";
+                        send+="  '.tictac -play X/O Xpos Ypos'\n plays the chosen letter at the given coordinate\n\n";
+                        send+="  ',tictac -clear'\n resets the board\n\n";
+                        send+="-----------------------\n";
+                        send+="'.random min max' : Chooses a random number in the given range\n\n";
+                        send+="-----------------------\n";
+                        send+="'.choose opt1 opt2 ... optn' : randomly chooses an option from the provided arguments";
+                        api.sendMessage(send, message.threadID);
+                        break;
 
-                //introduction to the botler
-                case ".about":
-                    api.sendMessage("Hello, I am a bot. Type '.help' for a listing of commands.", message.threadID);
-                    break;
+                    //introduction to the botler
+                    case ".about":
+                        api.sendMessage("Hello, I am a bot. Type '.help' for a listing of commands.", message.threadID);
+                        break;
 
-                //constructs a let me google that for you link and sends it
-                case ".google":
-                    var url = "https://lmgtfy.com/?q=";
-                    if (args.length>1){
-                        url+=args[1];
-                    }
-                    for (var g = 2; g < args.length; g++){
-                        url = url+"+"+args[g];
-                    }
-                    api.sendMessage(url, message.threadID);
-                    break;
+                    //constructs a let me google that for you link and sends it
+                    case ".google":
+                        var url = "https://lmgtfy.com/?q=";
+                        if (args.length>1){
+                            url+=args[1];
+                        }
+                        for (var g = 2; g < args.length; g++){
+                            url = url+"+"+args[g];
+                        }
+                        api.sendMessage(url, message.threadID);
+                        break;
 
-                //allows users to play tictactoe
-                case ".tictac":
-                    tictac(args, api, message.threadID);
-                    break;
+                    //allows users to play tictactoe
+                    case ".tictac":
+                        tictac(args, api, message.threadID, message.senderID);
+                        break;
 
-                //generates a random number within a certain range
-                case ".random":
-                    if (args.length < 3) api.sendMessage("USAGE: '.random min max'", message.threadID);
-                    else if (!Number.isInteger(parseInt(args[1])) || !Number.isInteger(parseInt(args[2]))) api.sendMessage("min and max must be integers", message.threadID);
-                    api.sendMessage(randInt(parseInt(args[1]), parseInt(args[2])).toString(), message.threadID);
-                    break;
+                    //generates a random number within a certain range
+                    case ".random":
+                        if (args.length < 3) api.sendMessage("USAGE: '.random min max'", message.threadID);
+                        else if (!Number.isInteger(parseInt(args[1])) || !Number.isInteger(parseInt(args[2]))) api.sendMessage("min and max must be integers", message.threadID);
+                        api.sendMessage(randInt(parseInt(args[1]), parseInt(args[2])).toString(), message.threadID);
+                        break;
 
-                //chooses a random value from a range of values given by users
-                case ".choose":
-                    if (args.length == 1) api.sendMessage("You didn't give me anything to choose from!", message.threadID);
-                    var choice = randInt(1, args.length)
-                    api.sendMessage(args[choice], message.threadID);
-                    break;
+                    //chooses a random value from a range of values given by users
+                    case ".choose":
+                        if (args.length == 1) api.sendMessage("You didn't give me anything to choose from!", message.threadID);
+                        var choice = randInt(1, args.length)
+                        api.sendMessage(args[choice], message.threadID);
+                        break;
+                }
             }
         });
     });
@@ -76,7 +79,7 @@ function randInt(min, max){
 }
 
 //parses the flags and other arguments of the .tictac command
-function tictac(cmd, api, thread){
+function tictac(cmd, api, thread, sender){
 
     //if the -print flag is given, print the board
     if (cmd[1] == "-print"){
@@ -87,7 +90,7 @@ function tictac(cmd, api, thread){
     else if (cmd[1] == "-play"){
         if (cmd[2] == "O" || cmd[2] == "X"){
             if ((cmd[3] > -1 && cmd[3] < 3) && (cmd[4] > -1 && cmd[4] < 3)){
-                addLetter(cmd[2], cmd[3], cmd[4], api, thread);
+                addLetter(cmd[2], cmd[3], cmd[4], api, thread, sender);
             }
             else{
                 api.sendMessage("```\nPlease specify a location thats on the board...", thread);
@@ -117,13 +120,22 @@ function printboard(api, thread){
 }
 
 //adds a letter to the board at the given position if it is a valid placement
-function addLetter(letter, xPos, yPos, api, thread){
+function addLetter(letter, xPos, yPos, api, thread, sender){
     var row = board[2-yPos].split(" ");
     if (row[xPos] == "-") row[xPos] = letter;
-    else api.sendMessage("There is already a letter at that position!", thread);
+    else{
+        api.sendMessage("```\nThere is already a letter at that position!", thread);
+        return;
+    }
+    if (sender == prev_player)
+    {
+        api.sendMessage("```\nYou can't play twice in a row, wait for someone else to play.", thread);
+        return;
+    }
     var new_row = row.join(" ");
     board[2-yPos] = new_row;
     moveCount++;
+    prev_player = sender;
 
     //print the board after making the move
     printboard(api, thread);
